b1c367b74cec2d064bc379b92fd757a0bab7efc1
Guillermo Pardo <guillermopardo88@gmail.com>
2023-06-01 09:37:14 -0300

    entrega Servidor express

--- src/app.js
+++ src/app.js
@@ -1,72 +1,83 @@
-import express from 'express';
-import ProductManager from './entregaTres.js';
-
+import express from "express";
+import productManager from "./entregaTres.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
 
-app.get('/api/products', async (req, res) => {
-    const products = await ProductManager.getProducts();
-    const limit = parseInt(req.query.limit);
-    if (!limit) {
-        return res.json(products)
+app.get("/api/products", async (req, res) => {
+  const products = await productManager.getProducts();
+  const limit = parseInt(req.query.limit);
+  if (!limit) {
+    return res.json(products);
+  } else {
+    if (limit > products.length) {
+      return res.status(400).json({
+        error: "Exced the limit of the products",
+      });
     } else {
-        if (limit > products.length) {
-            return res.status(400).json({
-                error: "Exced the limit of the products",
-            });
-        } else {
-            return res.status(200).json(products.slice(0, limit));
-        }
+      return res.status(200).json(products.slice(0, limit));
     }
+  }
 });
 
-app.get('/api/products/:pid', async (req, res) => {
-    const idRequested = parseInt(req.params.pid);
-    const userSearch = await ProductManager.getProductsById(idRequested);
-    if (userSearch) {
-        return res.status(200).json(userSearch);
-    } else {
-        return res.status(400).json({error: userSearch});
-    }
+app.get("/api/products/:pid", async (req, res) => {
+  const idRequested = parseInt(req.params.pid);
+  const userSearch = await productManager.getProductById(idRequested);
+  if (userSearch) {
+    return res.status(200).json(userSearch);
+  } else {
+    return res.status(400).json({ error: userSearch });
+  }
 });
 
 app.post("/api/products", async (req, res) => {
+  try {
     const productToAdd = req.body;
-    const products = await ProductManager.addProduct(productToAdd)
-    if (!products) {
-        res.status(200).json({ message: "Product added"});
-    } else {
-        res.status(400).json({ error: products})
+    const products = await productManager.addProduct(productToAdd);
+    res.status(200).json({ products });
+  } catch (error) {
+    if (
+      error.message === "Product info is not valid. Please chek de info bro"
+    ) {
+      return res.status(400).json({ message: error.message });
     }
+    if (
+      error.message ===
+      `The ${req.body.code} is already exist, use a diferent code rigth now!`
+    ) {
+      return res.status(409).json({ message: error.message });
+    }
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-
 app.put("/api/products/:pid", async (req, res) => {
-    const idProduct = parseInt(req.params.pid);
-    const newProduct = req.body;
-    const productModify = await ProductManager.updateProduct(idProduct, newProduct);
-    if(!productModify) {
-        res.status(200).json({ message: "Product modified"})
-    } else {
-        res.status(400).json({error: productModify});
-    }
+  const idProduct = parseInt(req.params.pid);
+  const newProduct = req.body;
+  const productModify = await productManager.updateProduct(
+    idProduct,
+    newProduct
+  );
+  if (!productModify) {
+    res.status(200).json({ message: "Product modified" });
+  } else {
+    res.status(400).json({ error: productModify });
+  }
 });
 
 app.delete("/api/products/:pid", async (req, res) => {
-    const idToDelete = parseInt(req.params.pid);
-    const productEliminated = await ProductManager.deleteProduct(idToDelete);
-    if (!productEliminated) {
-        res.status(200).json({ message: "Product eliminated"});
-    } else {
-        res.status(400).json({ error: productEliminated})
-    }
+  const idToDelete = parseInt(req.params.pid);
+  const productEliminated = await productManager.deleteProduct(idToDelete);
+  if (!productEliminated) {
+    res.status(200).json({ message: "Product eliminated" });
+  } else {
+    res.status(400).json({ error: productEliminated });
+  }
 });
-
--- src/entregaTres.js
+++ src/entregaTres.js
@@ -1,80 +1,80 @@
-import fs from 'fs';
-
+import fs from "fs";
 
 class ProductManager {
-    
-        constructor() {
-            this.path = './src/product.json';
-        }
-    
-        async addProduct(productInfo) {
-        const {title, description, price, thumbnail, code, stock} = productInfo;
-        if(!title || !description || !price || !thumbnail || !code || !stock){
-            return "Product info is not valid. Please chek de info bro"
-        }
-        const product = await this.getProducts()
-        if (product.some(product => product.code === code)){
-            return `The ${code} is already exist, use a diferent code rigth now!`
-        }
+  constructor(path) {
+    this.path = path;
+  }
 
-        const id = product[product.length - 1] ?.id + 1 || 1; 
-        const newProduct = { 
-            id, title, description, price, thumbnail, code, stock
-        };
-        product.push(newProduct);
-        await fs.promises.writeFile(this.path, JSON.stringify(product, null, 2));
-        return newProduct;
-        }
-    
-        async getProducts() {
-            try {
-            if(fs.existsSync(this.path)){        
-                    const data = await fs.promises.readfile(this.path, 'utf8');
-                    porduct = JSON.parse(data);
-                }	
-                await fs.promises.writeFile(this.path, JSON.stringify([]))
-                return [];
-            } catch (e){
-                throw new Error(e)
-        }}
-    
-        async getProductById(pid) {
-        let found = await this.getProducts();
-        let productFound = found.find(product => product.id === pid);
-        if(!productFound){
-            return `Product with ${pid} is not found`
-        }
-        return productFound;
-        }
-
-        
-
-        async updateProduct(pid, changes){
-            const product = await this.getProducts();
-            const productIndex = product.findIndex (product => product.id === pid);
-            if(productIndex === -1){
-                return `Product with ${pid} is not found`;
-            }
-            const productFound = product[productIndex];
-            if(productFound.code === code){
-                return `The code ${code} is already exist. Check out better!`
-            }
-            const productUpdate = {...productFound, ...changes}
-            product[productIndex] = productUpdate
-            fs.promises.writeFile(this.path, JSON.stringify(product, null, 2));
-        }
-
-        async deleteProduct(pid){
-            let product = await this.getProducts();
-            if(!product.some(product => product.id === pid)){
-                return `Product with id ${pid} is not found`
-            }
-            const newProduct = product.filter(product => product.id !== pid);
-            await fs.promises.writeFile(this.path, JSON.stringify(newProduct, null, 2));
-        }
+  async addProduct(productInfo) {
+    const { title, description, price, thumbnail, code, stock } = productInfo;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error ("Product info is not valid. Please chek de info bro");
     }
-    
-    
-    
-    
-    export default new ProductManager ("./src/productList.json")
+    const products = await this.getProducts();
+    console.log (products);
+    if (products.find((product) => product.code === code)) {
+      throw new Error (`The ${code} is already exist, use a diferent code rigth now!`);
+    }
+
+    const id = products[products.length - 1]?.id + 1 || 1;
+    const newProduct = {
+      id,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+    products.push(newProduct);
+    await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+    return newProduct;
+  }
+
+  async getProducts() {
+    try {
+        const data = await fs.promises.readFile(this.path, "utf8");
+        const products = JSON.parse(data);
+        return products;
+    } catch (e) {
+      console.log (e.message);
+        return [];
+    }
+  }
+
+  async getProductById(pid) {
+    let found = await this.getProducts();
+    let productFound = found.find((product) => product.id === pid);
+    if (!productFound) {
+      return `Product with ${pid} is not found`;
+    }
+    return productFound;
+  }
+
+  async updateProduct(pid, changes) {
+    const product = await this.getProducts();
+    const productIndex = product.findIndex((product) => product.id === pid);
+    if (productIndex === -1) {
+      return `Product with ${pid} is not found`;
+    }
+    const productFound = product[productIndex];
+    if (productFound.code === code) {
+      return `The code ${code} is already exist. Check out better!`;
+    }
+    const productUpdate = { ...productFound, ...changes };
+    product[productIndex] = productUpdate;
+    fs.promises.writeFile(this.path, JSON.stringify(product, null, 2));
+  }
+
+  async deleteProduct(pid) {
+    let product = await this.getProducts();
+    if (!product.some((product) => product.id === pid)) {
+      return `Product with id ${pid} is not found`;
+    }
+    const newProduct = product.filter((product) => product.id !== pid);
+    await fs.promises.writeFile(this.path, JSON.stringify(newProduct, null, 2));
+  }
+}
+
+export default new ProductManager("./src/product.json");
+
